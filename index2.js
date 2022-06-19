b2aea62942bd7d0957897fd26cbc2de4113d7c9c
Gaelle Briet <gaelle.briet@protonmail.com>
2022-06-19 19:35:48 +0200

    :'(

--- index2.js
+++ index2.js
@@ -1,29 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Toutes les lignes de pizza :
   let pizzas = document.querySelectorAll(".pizza");
-
   let boutonsMoins = document.querySelectorAll(".pizza-moins");
   let boutonsPlus = document.querySelectorAll(".pizza-plus");
-
-  let quantite = document.querySelectorAll("input"); //tableau
   let prixPizza = document.querySelectorAll(".pizza-prix"); //prixPizza[i].value = string
-  let pizzaSousTotal = document.querySelectorAll(".pizza-prix-total"); //tableau
   let prixTotal = document.querySelector(".prix-total"); // .textContent = string
   let sousTotal = 0;
 
   pizzas.forEach((pizza, i) => {
     boutonsMoins[i].addEventListener("click", (e) => {
-      quantite[i].value--;
-      pizzaSousTotal[i].textContent = quantite[i].value * prixPizza[i].value; //string
-      sousTotal = parseInt(pizzaSousTotal[i].textContent);
-      console.log(sousTotal);
+      //diminue la quantite de pizza et affiche la quantite dans l'input
+      document.querySelectorAll("input")[i].value--;
+      //affiche le montant de la pizza sélectionnée suivant la quantité voulue
+      document.querySelectorAll(".pizza-prix-total")[i].textContent =
+        document.querySelectorAll("input")[i].value * prixPizza[i].value;
+      // stocke la valeur du sous-total en nombre
+      sousTotal = parseInt(
+        document.querySelectorAll(".pizza-prix-total")[i].textContent
+      );
     });
 
     boutonsPlus[i].addEventListener("click", (e) => {
-      quantite[i].value++; //string
-      pizzaSousTotal[i].textContent = quantite[i].value * prixPizza[i].value; //string
-      sousTotal = parseInt(pizzaSousTotal[i].textContent);
-      console.log(sousTotal);
+      //augmente la quantite de pizza et affiche la quantite dans l'input
+      document.querySelectorAll("input")[i].value++;
+      //affiche le montant de la pizza sélectionnée suivant la quantité voulue
+      document.querySelectorAll(".pizza-prix-total")[i].textContent =
+        document.querySelectorAll("input")[i].value * prixPizza[i].value;
+      // stocke la valeur du sous-total en nombre
+      prixTotal = parseInt(
+        document.querySelectorAll(".pizza-prix-total")[i].textContent
+      );
     });
   });
 });
