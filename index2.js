12f2fe0c0389d03475a13db79179d28094ea9bc9
Gaelle Briet <gaelle.briet@protonmail.com>
2022-06-20 13:51:41 +0200

    fin

--- index2.js
+++ index2.js
@@ -1,39 +1,77 @@
+//fonction pour réinitialiser les inputs text (firefox garde en mémoire ce qui était inscrit)
+function resetElements() {
+  let inputs = document.querySelectorAll("input[type=text]");
+  for (let i = 0; i < inputs.length; i++) {
+    document.getElementsByTagName("input")[i].value = "0";
+    document.querySelector(".paiement-conteneur").style.visibility = "hidden";
+  }
+}
+
+// fonction pour afficher le bouton paypal/paiement si la commande est supérieure à zéro€
+function affichePaiement(total) {
+  if (parseInt(total.textContent) > 0) {
+    document.querySelector(".paiement-conteneur").style.visibility = "visible";
+  } else {
+    document.querySelector(".paiement-conteneur").style.visibility = "hidden";
+  }
+}
+
+// fonction qui affiche le recap de la commande
+function afficheRecap(i) {
+  if (quantite[i].value > 0) {
+    recapCommande[i] =
+      nomPizza[i].textContent + parseInt(quantite[i].value) + ".";
+  } else {
+    recapCommande[i] = "";
+  }
+  document.querySelector(".recap-commande").innerHTML = `
+  <p> Votre commande : </p>
+  <p> ${recapCommande.join("")} </p>
+  <p> Montant total :  ${total.textContent} € </p>
+  `;
+}
+
+let recapCommande = [];
+let quantite = document.querySelectorAll("input");
+let nomPizza = document.querySelectorAll("span");
+let total = document.querySelector(".prix-total");
+
 document.addEventListener("DOMContentLoaded", function () {
-  // Toutes les lignes de pizza :
   let pizzas = document.querySelectorAll(".pizza");
   let boutonsMoins = document.querySelectorAll(".pizza-moins");
   let boutonsPlus = document.querySelectorAll(".pizza-plus");
   let prixPizza = document.querySelectorAll(".pizza-prix");
+  let totalParPizza = document.querySelectorAll(".pizza-prix-total");
+  let totalCommande = 0;
 
   pizzas.forEach((pizza, i) => {
     boutonsMoins[i].addEventListener("click", (e) => {
+      if (quantite[i].value <= 0) return; // arrête l'éxécution pour garder une quantité positive de pizza
       //diminue la quantite de pizza et affiche la quantite dans l'input
-      document.querySelectorAll("input")[i].value--;
+      quantite[i].value--;
       //affiche le montant de la pizza sélectionnée suivant la quantité voulue
-      document.querySelectorAll(".pizza-prix-total")[i].textContent =
-        document.querySelectorAll("input")[i].value * prixPizza[i].value;
-      // stocke la valeur du sous-total en nombre
-      sousTotal = parseInt(
-        document.querySelectorAll(".pizza-prix-total")[i].textContent
-      );
-      // affiche le sous-total dans le prix total
-      // comment additionner chaque ligne et les afficher dans le total ?
-      document.querySelector(".prix-total").textContent = sousTotal;
+      totalParPizza[i].textContent = quantite[i].value * prixPizza[i].value;
+      //affiche le total de la commande
+      totalCommande -= parseInt(prixPizza[i].value);
+      total.textContent = totalCommande;
+      affichePaiement(total);
+      afficheRecap(i);
+      console.log(typeof quantite[i].value);
     });
 
     boutonsPlus[i].addEventListener("click", (e) => {
-      //augmente la quantite de pizza et affiche la quantite dans l'input
-      document.querySelectorAll("input")[i].value++;
-      //affiche le montant de la pizza sélectionnée suivant la quantité voulue
-      document.querySelectorAll(".pizza-prix-total")[i].textContent =
-        document.querySelectorAll("input")[i].value * prixPizza[i].value;
-      // stocke la valeur du sous-total en nombre
-      sousTotal = parseInt(
-        document.querySelectorAll(".pizza-prix-total")[i].textContent
-      );
-      // affiche le sous-total dans le prix total
-      // comment additionner chaque ligne et les afficher dans le total ?
-      document.querySelector(".prix-total").textContent = sousTotal;
+      quantite[i].value++;
+      totalParPizza[i].textContent = quantite[i].value * prixPizza[i].value;
+      totalCommande = totalCommande + parseInt(prixPizza[i].value);
+      total.textContent = totalCommande;
+      affichePaiement(total);
+      afficheRecap(i);
     });
+  });
+
+  let btnReset = document.querySelector(".reset-btn");
+  btnReset.addEventListener("click", () => {
+    window.location.reload();
+    resetElements();
   });
 });
